4b948fd62708adab6224484a79f1e27bb8106323
hitoshikyou <hitoshi.kyou@leatherball.jp>
2020-12-25 16:39:46 +0900

    Material UI installed

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "jest", "prettier"],
+  rules: {
+    // note you must disable the base rule as it can report incorrect errors
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
 
-function App() {
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+interface Props {}
+
+const App: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number>(0);
+  const [count2, setCount2] = useState<number>(0);
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>xs=12</Paper>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              aaa
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  setCount(count + 1);
+                }}
+              >
+                Add to Click( {count})
+              </Button>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              bbb
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  setCount2(count2 + 1);
+                }}
+              >
+                Add2 to Click({count2})
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
